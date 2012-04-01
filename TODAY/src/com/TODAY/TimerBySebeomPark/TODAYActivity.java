1ded7c0488bd9620fd6e8df3db82633d01398f71
freesora87 <freesora87@gmail.com>
2012-04-01 13:00:07 +0000

    package 이름을 확인해보시고, 각자 맞는 부분에서 작업해주세요 저는 timer 부분을 구현할 것이며 주영이는 parsing 부분 그리고 대영이 형은 UI 부분을 담당하게 될 것입니다.

---
+++ TODAY/src/com/TODAY/TimerBySebeomPark/TODAYActivity.java
@@ -0,0 +1,15 @@
+package com.TODAY.TimerBySebeomPark;
+
+import com.TODAY.Timer.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TODAYActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
