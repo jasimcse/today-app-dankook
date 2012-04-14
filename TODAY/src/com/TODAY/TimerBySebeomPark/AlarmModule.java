f54aa54bed8ca111355f548bb8d47c65155c7a2a
freesora87 <freesora87@gmail.com>
2012-04-14 08:14:37 +0000

    Some issues are changed 1. DatePicker will be added 2. TimePicker will be added

--- TODAY/src/com/TODAY/TimerBySebeomPark/AlarmModule.java
+++ TODAY/src/com/TODAY/TimerBySebeomPark/AlarmModule.java
@@ -6,10 +6,13 @@ import java.util.Calendar;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import android.app.Activity;
 
@@ -18,14 +21,19 @@ import android.app.Activity;
 
 public class AlarmModule {
 
-	void setTimerWithTask(Object baseClass, Object execClass, int time)
+	
+	// One time Alarm Moudle
+	// Need to add the repeating module because the alarm usuall repeat everyday.
+	
+	
+	void setTimerWithTask(Object baseClass, Class execClass, int time)
 	{
 		Log.i("here","here");
 		// first parameter is baseClass
 		
 		
 		// AlarmController 쩌ㅃ쩌ㅃ
-		Intent intent = new Intent((Activity)baseClass, execClass.getClass());
+		Intent intent = new Intent((Activity)baseClass, execClass);
 		
 		PendingIntent sender = PendingIntent.getBroadcast((Activity)baseClass,0,intent,0);
 		
@@ -44,8 +52,25 @@ public class AlarmModule {
 		// Confirm Message
 		
 		Toast.makeText(((Activity)baseClass), "Alarm has been registered", Toast.LENGTH_LONG).show();
-		
-		
+	
+	}
+	
+	
+
+	// This module is to select the time with date
+	// for Date Picker, it refers to http://developer.android.com/resources/tutorials/views/hello-datepicker.html
+	// for Time Picker, it refers to http://developer.android.com/reference/android/widget/TimePicker.html
+
+	void showTimePicker()			
+	{
+		TimePickerDialog.OnTimeSetListener mTimeSetListener = new OnTimeSetListener(){
+			
+			@Override
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
 	}
 	
 }
